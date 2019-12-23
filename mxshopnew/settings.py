7ef0e2a5a9c40656635760d53de2e6127fe38247
chenzao-coding <chenzaopython@163.com>
2019-12-23 11:09:45 +0800

    修复文档 docs 报错问题

--- mxshopnew/settings.py
+++ mxshopnew/settings.py
@@ -167,8 +167,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         # 'rest_framework.authentication.TokenAuthentication'
     ],
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
 }
 
 
